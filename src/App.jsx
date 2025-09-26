0eeccfa6eaed7159d82363582fbb2ec3672687c1
afferio646 <khall@dbmgconsulting.com>
2025-09-26 10:35:56 -0400

    Update App.jsx
    chnages

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import ContactSupportModal from './components/ContactSupportModal.jsx';
 
 
 // --- SECURE API KEY HANDLING ---
-const apiKey = import VITE_GEMINI_API_KEY;
+const apiKey = VITE_GEMINI_API_KEY;{"AIzaSyCc99ko0wH88tHzBh-sCaEa0_txm5n-p3Y"}
 
 // --- Helper Components ---
 
